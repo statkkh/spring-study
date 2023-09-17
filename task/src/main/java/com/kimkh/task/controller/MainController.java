3b3051e2224ecc062686abc1bdce4fab98fcb3ca
khkim <stakkh4@gmail.com>
2023-09-17 17:06:26 +0900

    class - 과제 -2

--- task/src/main/java/com/kimkh/task/controller/MainController.java
+++ task/src/main/java/com/kimkh/task/controller/MainController.java
@@ -23,7 +23,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/v1")
 
-// @RequiredArgsConstructor
+@RequiredArgsConstructor
 public class MainController {
     
     @GetMapping("task/{taskNumber}")
